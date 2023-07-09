e00139bebe5303856e09fce2fa80c1c079907151
yıldız <mesel4461@gmail.com>
2023-07-09 19:49:47 +0300

    token ok

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -36,7 +36,12 @@ export const login = async (req, res, next) => {
       process.env.JWT
     );
     const { password, isAdmin, ...otherDetails } = user._doc;
-    res.status(200).json({ ...otherDetails });
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ ...otherDetails });
   } catch (err) {
     next(err);
   }
