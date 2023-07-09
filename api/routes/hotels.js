de6ae4296e3b4085dffb4054cae7e2fa511314cd
yıldız <mesel4461@gmail.com>
2023-07-09 15:21:54 +0300

    create put

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -14,6 +14,15 @@ router.post("/", async (req, res) => {
   }
 });
 // UPDATE
+router.put("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // DELETE
 // GET
 // GET ALL
