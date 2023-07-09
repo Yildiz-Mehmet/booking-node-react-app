add3b71e8e3362cdab9b7b77615f3ba4416af123
yıldız <mesel4461@gmail.com>
2023-07-09 16:15:26 +0300

    up

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -48,8 +48,13 @@ router.get("/:id", async (req, res) => {
 });
 // GET ALL
 router.get("/", async (req, res, next) => {
+  const failed = true;
+  const err = new Error();
+  err.status = 404;
+  err.message = "Sorry not found!";
+  if (failed) return next(err);
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.findById("adcacasc");
     res.status(200).json(hotels);
   } catch (err) {
     next(err);
