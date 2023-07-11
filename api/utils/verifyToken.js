ffd21bcb696d6565fb0f0ad859f3e8d82460e6eb
yıldız <mesel4461@gmail.com>
2023-07-11 21:50:37 +0300

    as

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken";
-import { createError } from "./error.js";
+import { createError } from "../utils/error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(createError(401, "You are not authentication!"));
+    return next(createError(401, "You are not authenticated!"));
   }
+
   jwt.verify(token, process.env.JWT, (err, user) => {
     if (err) return next(createError(403, "Token is not valid!"));
     req.user = user;
@@ -22,6 +23,7 @@ export const verifyUser = (req, res, next) => {
     }
   });
 };
+
 export const verifyAdmin = (req, res, next) => {
   verifyToken(req, res, next, () => {
     if (req.user.isAdmin) {
